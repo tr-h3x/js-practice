ae1b8c1d5d020089bc089ddec80a4ac82050ff28
tr_h3x <travis.hetzel@gmail.com>
2022-04-04 14:18:36 -0700

    Create standard HTML boiler plate, css, js, and md
    Follow along tutorial video. Take notes and focus on
    javascript keypoints.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # js-practice
 Follow YT video "Javascript  Tutorial For Beginners" by Programming with Mosh
+
+https://www.youtube.com/watch?v=W6NZfCO5SIk
+
+Supplement TOP javascript learning with this video. Follow along and learn.
---
+++ myScript.js
@@ -0,0 +1,28 @@
+// This is my first js code
+
+// console.log("Hello, World.");
+
+let name =  "Travis";
+console.log(name);
+
+// variables cannot be a reserved key word
+// name should be meaningful
+// cannot start with a number
+// cannot contain space or hyphen (use camelCase)
+// variable names are case sensitive 
+// can declare multiple variables at once
+
+let interestRate = 0.3
+interestRate = 1
+console.log(interestRate);
+
+// to lock in value, use "const" syntax
+// ie "const interestRate == 0.3"
+// constants can NOT be reassigned
+
+let name = "Travis"; //string literal
+let age = 30; // number literal
+let isApproved = false; //boolean literal
+let firstName; //creates an undefined variable
+let selectedColor = null;
+
