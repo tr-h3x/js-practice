4c2c9504eef3e31fabee7b625bf3f999b7ca81a7
tr_h3x <travis.hetzel@gmail.com>
2022-04-04 15:15:02 -0700

    Learn js functions, take notes, practice.

--- myScript.js
+++ myScript.js
@@ -57,7 +57,42 @@ console.log(person.name);
 
 
 //ARRAYS
+//arrays are data structures used to represent a list of items
+let selectedColors = ["red", "blue"];
+//console.log(selectedColors[0]);
+selectedColors[2] = "green" //adds additional color to index 2 in list
+console.log(selectedColors)
 
+//arrays are dynamic in length and contained object types
+//instead of "green" for [2], a number, string, etc could be used. 
 
+//FUNCTIONS
 
+function greet() {
+    console.log("Hello, world");
+}
+//functions do not end with semi-colon(;). But used when calling functions as statements:
 
+greet();
+
+//function with input:
+function greet(name) { //"name" is the parameter of the funciton
+    console.log("Hello, " + name)
+}
+
+greet("TRWH"); //"TRWH" is an argument of the function, which supplies the parameter
+greet("Samantha");
+
+//function w/ multiple parameters
+function formalGreet(name, lastName) {
+    console.log("Hello, " + name + " " + lastName + ".")
+}
+formalGreet("TR", "Hex");
+
+//function that calculates value
+function square(number) {
+    return number * number;
+}
+let number = square(10);
+console.log(number);
+console.log(square(3)); //cleaner version
